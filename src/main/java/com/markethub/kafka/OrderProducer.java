77f39a7596b69fd68c32de92a8a07e7b2b838f50
Vishal Kumar <official.vishalk1999@gmail.com>
2026-04-21 21:15:00 +0530

    feat: add kafka order producer

---
+++ src/main/java/com/markethub/kafka/OrderProducer.java
@@ -0,0 +1,18 @@
+package com.markethub.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public OrderProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendOrderEvent(String message) {
+        kafkaTemplate.send("order-events", message);
+    }
+}
